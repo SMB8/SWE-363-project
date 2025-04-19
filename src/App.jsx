7328e66bd47d6554f7a90d575bbc6d680026da86
Saifullah Bukhari <bukhari.saifullah8@gmail.com>
2025-04-19 21:28:09 +0300

    Adding event page and Check event page

--- src/App.jsx
+++ src/App.jsx
@@ -2,15 +2,15 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg';
 import viteLogo from '/vite.svg';
 import Header from './codeParts/Header';
+import CreatingEvent from './codeParts/CreatingEvent';
+import CheckEvent from './codeParts/CheckEvent';
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-      <div>
-        <Header/>
-      </div>
+      <CheckEvent/>
     </>
   )
 }
