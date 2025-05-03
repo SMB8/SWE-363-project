786393efc049683e1260ad62c92bca300733a197
“faisal <faisalsamialshalan>
2025-05-03 20:13:52 +0300

    connect front to back

--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,10 @@
-import express from 'express';
-import mongoose from 'mongoose';
-import cors from 'cors';
-import dotenv from 'dotenv';
-import process from 'node:process';
-import authRoutes from './routes/auth.js';
+import express from "express";
+import mongoose from "mongoose";
+import cors from "cors";
+import dotenv from "dotenv";
+import process from "node:process";
+import authRoutes from "./routes/auth.js";
+import eventsRoutes from "./routes/events.js";
 
 dotenv.config();
 const app = express();
@@ -12,21 +13,21 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 app.use((req, res, next) => {
-    console.log(`${req.method} ${req.url}`);
-    next();
+  console.log(`${req.method} ${req.url}`);
+  next();
 });
 // MongoDB connection
-mongoose.connect(process.env.MONGO_URI)
-    .then(() => console.log('MongoDB connected successfully'))
-    .catch((err) => console.error('MongoDB connection error:', err));
+mongoose
+  .connect(process.env.MONGO_URI)
+  .then(() => console.log("MongoDB connected successfully"))
+  .catch((err) => console.error("MongoDB connection error:", err));
 
-
-app.use('/api/auth', authRoutes);
+app.use("/api/auth", authRoutes);
+app.use("/api/events", eventsRoutes);
 //todo, middleware so all routes are protected and no access to the api without a token
-
 
 // Start the server
 const PORT = 54321;
 app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`);
+  console.log(`Server running on port ${PORT}`);
 });
